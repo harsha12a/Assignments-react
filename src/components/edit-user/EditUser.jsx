210efb5b9332afc4ba625b411814908dad906bf3
harsha12a <141937574+harsha12a@users.noreply.github.com>
2024-06-22 17:03:24 +0530

    add

--- src/components/edit-user/EditUser.jsx
+++ src/components/edit-user/EditUser.jsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 function EditUser() {
     let {curr,setcurr}=useContext(UserLoginContext)
-    let {register,handleSubmit,setValue}=useForm()
+    let {register,handleSubmit,setValue,formState:{errors}}=useForm()
     let navigate=useNavigate()
     async function onsave(obj){
         let res=await fetch(`http://localhost:3000/users/${curr.id}`,{
@@ -28,22 +28,27 @@ function EditUser() {
             <div className="mb-3">
               <label className='form-label' htmlFor="username">Username</label>
               <input className='form-control' type="text" {...register('username',{required:true})} value={setValue('username',curr?.username)} id="username" />
+              {errors.username?.type==='required' && <p className='text-danger'>*Username is required</p>}
             </div>
             <div className="mb-3">
               <label className='form-label' htmlFor="password">Password</label>
               <input className='form-control' type="password" {...register('password',{required:true})} value={setValue('password',curr?.password)} disabled id="password" />
+              {errors.password?.type==='required' && <p className='text-danger'>*Password is required</p>}
             </div>
             <div className="mb-3">
               <label className='form-label' htmlFor="email">Email</label>
               <input className='form-control' type="email" {...register('email',{required:true})} value={setValue('email',curr?.email)} id="email" />
+              {errors.email?.type==="required" && <p className='text-danger'>*Email is required</p>}
             </div>
             <div className="mb-3">
               <label htmlFor="contact" className="form-label">Contact number</label>
               <input type="number" {...register('contact',{required:true})} value={setValue('contact',curr?.contact)} id="contact" className="form-control" />
+              {errors.contact?.type==="required" && <p className='text-danger'>*Contact number is required</p>}
             </div>
             <div className="mb-3">
               <label htmlFor="profile" className="form-label">Paste profile img url</label>
               <input type="url" {...register('profile',{required:true})} id="profile" disabled value={setValue('profile',curr?.profile)} className="form-control" />
+              {errors.profile?.type==="required" && <p className='text-danger'>*Profile url is required</p>}
             </div>
             <button type="submit" className='btn btn-success'>Save</button>
           </form>
